17be652d812a7a9625517de9d22fe367da218de5
Ivan Maidana <imaidana@DarkJonux.local>
2023-07-01 14:19:46 -0300

    commit

--- React/src/Formulario.jsx
+++ React/src/Formulario.jsx
@@ -12,9 +12,16 @@ export const Formulario = () => {
     const [spanSuccess, setSpanSuccess] = useState('');
 
     let count = null;
+    const enable = (user, pass) => {
+        if (!user.includes('.')) {return 'error'}
+        if (!user.includes('@')) {return 'error'}
+        if (!pass.length >= 8) { return 'error'}
+    }
+
+    const validateEnable = enable(name,password)
 
     const validarFormulario = () => {
-        if (name === '' && name === ' ') {
+        if (name.includes('') && name.includes(' ')) {
             const msj = 'Ingrese un valor para el campo de Nombre';
             setNameError(msj);
             return false;
@@ -45,12 +52,12 @@ export const Formulario = () => {
         } else {
             setPasswordError('');
         }
-   
+
         if (password.length < 8) {
             const msj = 'Ingrese un valor con más de 8 caracteres para el campo de Contraseña';
             setPasswordError(msj);
         } else {
-            if (Boolean(users.username.find(user => user===name)) && Boolean(pass.password.find(pw => pw===password))){
+            if (Boolean(users.username.find(user => user === name)) && Boolean(pass.password.find(pw => pw === password))) {
 
                 const nameCapitalize1 = name.charAt(0).toUpperCase();
                 const nameCapitalize2 = name.slice(1, count);
@@ -73,7 +80,7 @@ export const Formulario = () => {
                         <h1 className="text-center mb-5">Iniciar Sesión</h1>
                         <div className="mb-3">
                             <label htmlFor="nombre" className="form-label">Nombre</label>
-                            <input type="text" autoComplete="current-password" className="form-control" id="nombre" placeholder="Ingrese su nombre de Usuario o Email" value={name} onChange={(e) => setName(e.target.value)} />
+                            <input type="text" autoComplete="current-password" className="form-control" id="nombre" placeholder="Ingrese su Email" value={name} onChange={(e) => setName(e.target.value)} />
                             <div id="nombreERROR" className="form-text text-danger">{nameError}</div>
                         </div>
                         <div className="mb-3">
@@ -81,7 +88,7 @@ export const Formulario = () => {
                             <input type="password" autoComplete="current-password" className="form-control" id="password" placeholder="Ingrese su contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
                             <div id="passwordERROR" className="form-text text-danger">{passwordError}</div>
                         </div>
-                        <button type="button" id="btn-formulario" className="btn btn-primary" onClick={validarFormulario}>Iniciar Sesión</button>
+                        <button type="button" id="btn-formulario" className="btn btn-primary" onClick={validarFormulario} disabled={validateEnable}>Iniciar Sesión</button>
                     </form>
                 </div>
             </div>
--- React/src/Register.jsx
+++ React/src/Register.jsx
@@ -1,11 +1,28 @@
 import { Link } from "react-router-dom"
 
 export const Register = () => {
-    alert("En construcción")
     return (
-        <div>
+        <div className="container py-5 vh-100">
             <Link className='fs-3 text-decoration-none' to={'/'}>⬅</Link>
-            <h1>je!</h1>
+            <div className="row mb-5">
+                <div className="col-md-6 offset-md-3">
+                    <form>
+                        <h1 className="text-center mb-5">Sign Up</h1>
+                        <div className="mb-3">
+                            <label htmlFor="nombre" className="form-label">Nombre</label>
+                            <input type="text" autoComplete="current-password" className="form-control" id="nombre" placeholder="Registre su nombre de Usuario o Email"  required={true}/>
+                            <div id="nombreERROR" className="form-text text-danger"></div>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Contraseña</label>
+                            <input type="password" autoComplete="current-password" className="form-control" id="password" placeholder="Ingrese la contraseña" />
+                            <div id="passwordERROR" className="form-text text-danger"></div>
+                        </div>
+                        <button type="button" id="btn-formulario" className="btn btn-primary">Sign Up!</button>
+                    </form>
+                </div>
+            </div>
+            <h1 id="span" className="text-center"></h1>
         </div>
     )
 }
